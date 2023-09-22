99da490ad85919aa6fe6bea43951a1a7b212c287
Sarath <pandrankisarath@gmail.com>
2023-09-22 15:49:28 +0530

    java int boundaries

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+
+        int upperbond = (int)32767;
+        int lowerbond = (int)-32768;
+        System.out.println(upperbond);
+        System.out.println(lowerbond);
+    }
+}
